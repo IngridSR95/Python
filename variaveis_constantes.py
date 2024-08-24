c6a912fc0b3e41158d454438ffb7ebe4952bf965
IngridSR95 <sringrid95@gmail.com>
2024-08-23 21:17:32 -0300

    Variáveis e Constantes

---
+++ variaveis_constantes.py
@@ -0,0 +1,13 @@
+nome = "ingrid"
+idade = 19
+print(nome,idade)
+
+nome, idade = "maria", 18
+print(nome, idade)
+
+limite_saque_diario = 1000
+print(limite_saque_diario)
+
+#Constante tem valor imutável
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+print(BRAZILIAN_STATES)
