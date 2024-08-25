c88df131a661c06e1274a4edf8433271fea148cb
IngridSR95 <sringrid95@gmail.com>
2024-08-25 15:21:52 -0300

    Conversão de Variáveis

---
+++ Exercicios/convertendo_tipos.py
@@ -0,0 +1,40 @@
+#int para float
+preco = 10
+print(preco)
+
+preco = float(preco)
+print(preco)
+
+preco = 10 / 2
+print(preco)
+
+print("\n")
+
+#float para int
+preco = 10.30
+print(preco)
+
+preco = int(preco)
+print(preco)
+print("\n")
+
+#Conversão por divisão
+preco = 10
+print(preco)
+
+print(preco / 2)
+
+print(preco // 2) #preserva o número ineiro
+print("\n")
+
+#Numérico para str
+preco = 10.50
+idade = 28
+
+print(str(preco))
+
+print(str(idade))
+
+texto = f'idade {idade} preço {preco}'
+print(texto)
+print("\n")
