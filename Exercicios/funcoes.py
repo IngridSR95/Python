ffc0161cd8970f5280435fa2637691e915925a73
IngridSR95 <sringrid95@gmail.com>
2024-09-01 16:19:14 -0300

    Introdução à Funções

---
+++ Exercicios/funcoes.py
@@ -0,0 +1,46 @@
+'''FUNÇÕES'''
+def exibir_mensagem():
+    print("Olá Mundo!\n")
+
+def exibir_mensagem_2(nome):
+    print(f"Seja bem-vindo, {nome}!\n")
+
+def exibir_mensagem_3(nome = "Antônio"):
+    print(f"Seja bem-vindo, {nome}!\n")
+
+exibir_mensagem()
+exibir_mensagem_2("Pedro")
+exibir_mensagem_3()
+exibir_mensagem_3(nome = "Matheus")
+
+#Lista que retorna números:
+
+def calcular_total(numero):
+    return sum(numero)
+
+def retorns_antecessor_sucessor(numero):
+    antecessor = numero - 1
+    sucessor = numero + 1
+
+    return antecessor, sucessor
+
+print(calcular_total([10, 20, 34]))
+print(retorns_antecessor_sucessor(10))
+
+#ARGUMENTOS NOMEADOS
+def salvar_carro(marca, modelo, ano, placa):
+    # salva carro no banco de dados...
+    print(f"Carro inserido com sucesso! {marca}/{modelo}/{ano}/{placa}")
+
+salvar_carro ("Fiat", "Palio", 1999, "ABC-1234\n")
+salvar_carro(marca="Fiat", modelo="Palio", ano=1999, placa="ABC-1234\n")
+salvar_carro(**{"marca": "Fiat", "modelo": "Palio", "ano": 1999, "placa": "ABC-1234\n"}) #dicionário
+
+#*args e **kwargs (tupla e dicionário)
+def exibir_poema (data_extenso, *args, **kwargs):
+    texto = "\n".join(args)
+    meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
+    mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
+    print(mensagem)
+    
+exibir_poema ("Dom, 01 set 2024", "Zen of Python", "Beautiful is better than ugly.", autor="Tim Peters", ano=1999)
