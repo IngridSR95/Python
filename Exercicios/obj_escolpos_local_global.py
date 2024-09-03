5c89b015d28f05ed1405b03ea972883f3cdfb5f3
IngridSR95 <sringrid95@gmail.com>
2024-09-03 11:55:37 -0300

    ESCOPO LOCAL E ESCOPO GLOBAL

--- Exercicios/obj_escolpos_local_global.py
+++ Exercicios/obj_escolpos_local_global.py
@@ -18,3 +18,17 @@ exibir_resultado(10, 5, subtrair)
 op = somar
 print(op(1, 19))
 
+'''ESCOPO LOCAL E ESCOPO GLOBAL'''
+#esta prática deve ser evitada
+salario = 2000
+
+def salario_bonus(bonus, lista):
+    global salario
+    lista.append(2)
+    salario += bonus
+    return salario
+
+lista = [1] #por referência, obj mutável
+novo_salario = salario_bonus(500, lista)
+print(novo_salario)
+print(lista)
