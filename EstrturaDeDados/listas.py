47ffa59fb586ca80c37e34871972f15247d5fae1
IngridSR95 <sringrid95@gmail.com>
2024-09-13 21:51:24 -0300

    listas 1

---
+++ EstrturaDeDados/listas.py
@@ -0,0 +1,12 @@
+#Criando Listas
+frutas = ["laranja", "maca", "uva"]
+
+letras = list("python") #elemento iteravel
+
+numeros = list(range(10)) #0 a 9
+
+carro = ["Ferrari", "F8", 42000, 2020, 2900, "São Paulo", True]
+
+# Acesso Direto
+frutas[0]
+frutas[2]
