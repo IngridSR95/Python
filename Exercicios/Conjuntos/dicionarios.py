f9c3667ac56cd618cc38d288acb005b28ca35e6c
IngridSR95 <sringrid95@gmail.com>
2024-09-26 21:30:49 -0300

    Dicionários

---
+++ Exercicios/Conjuntos/dicionarios.py
@@ -0,0 +1,11 @@
+''' DICIONARIO 
+conjunto não-ordenado de pares chave:valor,
+são delimitados por {} e contém uma lista de pares.
+'''
+pessoa = {"nome":"Ingrid", "idade":19}
+
+pessoa["telefone"] = "4002-8922"
+
+print(pessoa)
+
+# Dicionários Alinhados
